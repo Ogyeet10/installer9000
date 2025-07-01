ec2402c1d552328eab6c7ecd4fe4a235f3926fed
Aidan Leuenberger <71840866+Ogyeet10@users.noreply.github.com>
2025-06-30 22:14:24 -0500

    redirected to new sw win script

--- launcher.ps1
+++ launcher.ps1
@@ -1,9 +1,11 @@
 # launcher.ps1
+# Check if running as administrator, if not relaunch as admin
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
-$scriptUrl = 'https://raw.githubusercontent.com/Ogyeet10/installer9000/main/conf.ps1'
+# Download and execute the remote script
+$scriptUrl = 'https://github.com/Ogyeet10/installer9000/raw/refs/heads/main/sw-win.ps1'
 $scriptContent = (New-Object Net.WebClient).DownloadString($scriptUrl)
 Invoke-Expression $scriptContent
